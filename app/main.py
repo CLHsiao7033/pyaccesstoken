b73398bf4776f684d10e52689ef4b9de52b04677
CLHsiao7033 <110364009+CLHsiao7033@users.noreply.github.com>
2024-04-10 22:01:36 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,19 +2,13 @@ import os
 import time
 import json
 import datetime
-import subprocess
 from flask import Flask
 from flask import jsonify
-from flask import request
-
 
 app = Flask(__name__)
 
 @app.route("/azure/accessToken", methods = ['GET'])
 def azure_accessToken():
-    #proc = subprocess.Popen(["az", "account", "get-access-token", "--query", "accessToken", "--output", "tsv"], stdout=subprocess.PIPE, shell=True)
-    #(out, err) = proc.communicate()
-    #print("program output:", out)
     ans = os.popen('az account get-access-token --query accessToken --output tsv').read()
     print(ans)
     return {
